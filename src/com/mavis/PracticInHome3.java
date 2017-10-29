c4c8380ccb474bada7f17a41ef9e35b8955a239c
Mavis <angel253698@gmail.com>
2017-10-29 12:45:54 +0800

    10/29practic

---
+++ src/com/mavis/PracticInHome3.java
@@ -0,0 +1,19 @@
+package com.mavis;
+
+public class PracticInHome3 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		// sum = start with 
+		int i;
+		
+		for (i = 1; i <= 100; i++) {
+		// for = ´`Àô
+			
+			sum += i; 
+		}
+		
+		System.out.println("1 + 2 + .... + 99 + 100 =" + sum); 
+	}
+
+}
