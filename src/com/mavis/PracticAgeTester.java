d0a05875b27e750d305a0d67d32600673cdf2103
Mavis <angel253698@gmail.com>
2017-12-17 15:25:20 +0800

    practic Age Tester

---
+++ src/com/mavis/PracticAgeTester.java
@@ -0,0 +1,30 @@
+package com.mavis;
+
+import java.util.Scanner;
+
+public class PracticAgeTester {
+	
+	public static void main(String[] args) {
+		System.out.println("Please type in your age:");
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.nextLine();
+		int age = Integer.parseInt(line);
+		if (age < 7) {
+			System.out.println("讀幼稚園嗎??");
+		} else {
+			if (age < 12) {
+				System.out.println("讀國小嗎??");
+			} else {
+				if (age < 15) {
+					System.out.println("讀國中嗎??");
+				} else {
+					if (age < 18) {
+						System.out.println("讀高中嗎??");
+					} else {
+						System.out.println("找工作還念大學了?");
+					}
+				}
+			}
+		}
+	}
+}
--- src/com/mavis/PraticBubble.java
+++ src/com/mavis/PraticBubble.java
@@ -3,8 +3,9 @@ package com.mavis;
 public class PraticBubble {
 
 	public static void main(String[] args) {
-		
+
 		int n[] = { 51, 23, 6, 65, 15 };
+
 		for (int i = 0; i < n.length - 1; i++) {
 			for (int k = (i + 1); k < n.length; k++) {
 				if (n[i] > n[k]) {
