37e7c6594c904e80c0df2ad6d33ce9852edfe6d2
Mavis <angel253698@gmail.com>
2017-12-17 14:49:26 +0800

    practic bubble

---
+++ src/com/mavis/PraticBubble.java
@@ -0,0 +1,21 @@
+package com.mavis;
+
+public class PraticBubble {
+
+	public static void main(String[] args) {
+		
+		int n[] = { 51, 23, 6, 65, 15 };
+		for (int i = 0; i < n.length - 1; i++) {
+			for (int k = (i + 1); k < n.length; k++) {
+				if (n[i] > n[k]) {
+					int tmp = n[i];
+					n[i] = n[k];
+					n[k] = tmp;
+				}
+			}
+		}
+		for (int num : n) {
+			System.out.print(num + " ");
+		}
+	}
+}
