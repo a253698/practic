74ce8162f3de086ffe40fa260339fa8e09dff950
Mavis <angel253698@gmail.com>
2017-12-18 12:46:40 +0800

    PracticDice01

--- src/com/mavis/PracticDice.java
+++ src/com/mavis/PracticDice.java
@@ -1,11 +1,13 @@
 package com.mavis;
 
 public class PracticDice {
-	
+
 	public static void main(String[] args) {
-
 		
-
+		int a1;
+		int a2;
+		int a3;
+		
 		int a[] = new int[3];
 		a[0] = 4;
 		a[1] = 5;
@@ -14,5 +16,4 @@ public class PracticDice {
 			System.out.println(a[i]);
 		}
 	}
-
 }
