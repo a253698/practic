d25c23e07fb507a86a03081938150a9bd44ac339
Mavis <angel253698@gmail.com>
2017-12-18 13:07:10 +0800

    PracticEven

---
+++ src/com/mavis/PracticEven.java
@@ -0,0 +1,15 @@
+package com.mavis;
+
+public class PracticEven {
+
+	public static void main(String[] args) {
+		int n = 25;
+		if (n % 2 != 0) {
+			System.out.println("是奇數");
+		} else {
+			if (n % 2 == 0) {
+				System.out.println("是偶數");
+			}
+		}
+	}
+}
