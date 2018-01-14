1826dee9c02b8745c39abede42485e61253e8b0d
Mavis <angel253698@gamil.com>
2018-01-14 19:28:00 +0800

    PracticPokerCard

--- src/com/mavis/pokercard/PracticShuffle2.java
+++ src/com/mavis/pokercard/PracticShuffle2.java
@@ -1,36 +1,27 @@
 package com.mavis.pokercard;
 
-public class PracticShuffle2 {
-	public static void main(String[] args) {
-		final int N = 10;
-		int[] poker = new int[N + 1];
+import java.util.Random;
 
-		int count = list.length;  
-        //结果集  
-        int[] resultList = new int[count];  
-        //用一个LinkedList作为中介  
-        LinkedList<Integer> temp = new LinkedList<Integer>();  
-        //初始化temp  
-        for(int i = 0; i<count; i++){  
-            temp.add((Integer)list[i]);  
-        }  
-        //取数  
-        Random rand = new Random();  
-        for(int i = 0;i<count; i++){  
-            int num = rand.nextInt(count - i);  
-            resultList[i] = (Integer) temp.get(num);  
-            temp.remove(num);  
-        }  
-          
-        return resultList;  
-    }  
-      
-    public static void main(String[] args) {  
-        int[] list = {0,1,2,3,4,5,6,7,8,9};  
-        int[] result = RandomNumber.getList(list);  
-        for(int i=0; i<result.length; i++){  
-            System.out.print("["+result[i]+"] ");  
-        }  
-    }  
-}  
+public class PracticShuffle2 {
+	
+	public static void main(String[] args) {
+		int N = 9;
+		Random r = new Random();
+		int a[] = new int[N + 1];
+
+		for (int i = 1; i <= N; i++)
+			a[i] = i;
+
+		for (int i = 1; i <= N; i++) {
+			int j = (int) (r.nextInt(10));
+
+			int tmp = a[i];
+			a[i] = a[j];
+			a[j] = tmp;
+		}
+		for (int p : a) {
+			System.out.print(a[p] + " ");
+		}
+
+	}
 }
