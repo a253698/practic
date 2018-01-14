7d7480284d466adfcdc210b69c689ac616bb0a14
Mavis <angel253698@gamil.com>
2018-01-14 19:50:50 +0800

    PracticDiceRollerCount6

--- src/com/mavis/PracticDiceRoller.java
+++ src/com/mavis/PracticDiceRoller.java
@@ -5,12 +5,20 @@ import java.util.Random;
 public class PracticDiceRoller {
 
 	public static void main(String[] args) {
+		
+			int count = 0;
+		
 		for (int i = 1; i < 10; i++) {
 			Random r = new Random();
 			int n1 = r.nextInt(6) + 1;
 			int n2 = r.nextInt(6) + 1;
 			int n3 = r.nextInt(6) + 1;
 			int n4 = r.nextInt(6) + 1;
+			
+			if(n1 == 6) { count++;}
+			if(n2 == 6) { count++;} 
+			if(n3 == 6) { count++;} 
+			if(n4 == 6) { count++;} 
 
 			if (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4) {
 				System.out.println(n1 + " " + n2 + " " + n3 + " " + n4);
@@ -19,9 +27,12 @@ public class PracticDiceRoller {
 				System.out.println(n1 + " " + n2 + " " + n3 + " " + n4);
 				break;
 			}
-
+			
+		}
+		
+		System.out.println("6點出現的次數:"+ count);
 		}
 
 	}
 
-}
+
