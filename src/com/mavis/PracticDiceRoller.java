022a34a91a482ab5c4b9aadd45d9b8a6c1e81bd7
Mavis <angel253698@gamil.com>
2018-01-14 20:14:30 +0800

    PracticDiceRollerArrayCount6

--- src/com/mavis/PracticDiceRoller.java
+++ src/com/mavis/PracticDiceRoller.java
@@ -5,57 +5,71 @@ import java.util.Random;
 public class PracticDiceRoller {
 
 	public static void main(String[] args) {
-		
-		int a[] = new int [ 6 +1];
-		for(int k=1; k<=10; k++) {
+		int C = 0;
+		int M = 6;
+		// M=六面
+		int a[] = new int[M + 1];
+		for (int k = 1; k <= 5; k++) {
+			// k=骰幾次
 			a[k] = k;
-			
-			for(int j = 1; j <= 4; j++) {
-				
+
+			for (int j = 1; j <= 4; j++) {
+				// j=幾顆骰子
+
 				Random r = new Random();
 				a[k] = r.nextInt(6) + 1;
 				
+//				if(a[k] =! a[k]) {
+//					System.out.print(a[k]);
+//				}
+				
 				System.out.print(a[k]);
-			
+				
+				if(a[k] == 6) {
+					C++;
+				}
+
 			}
+			
 			System.out.print(" ");
+			
 		}
-		
-		
-//
-//		int count = 0;
-//
-//		for (int i = 1; i < 10; i++) {
-//
-//			Random r = new Random();
-//
-//			int n1 = r.nextInt(6) + 1;
-//			int n2 = r.nextInt(6) + 1;
-//			int n3 = r.nextInt(6) + 1;
-//			int n4 = r.nextInt(6) + 1;
-//
-//			if (n1 == 6) {
-//				count++;
-//			}
-//			if (n2 == 6) {
-//				count++;
-//			}
-//			if (n3 == 6) {
-//				count++;
-//			}
-//			if (n4 == 6) {
-//				count++;
-//			}
-//
-//			if (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4) {
-//				System.out.println(n1 + " " + n2 + " " + n3 + " " + n4);
-//				continue;
-//			} else {
-//				System.out.println(n1 + " " + n2 + " " + n3 + " " + n4);
-//				break;
-//			}
-//		}
-//		System.out.println("6點出現的次數:" + count);
+		System.out.print("6出現次數:" + C);
+
+		//
+		// int count = 0;
+		//
+		// for (int i = 1; i < 10; i++) {
+		//
+		// Random r = new Random();
+		//
+		// int n1 = r.nextInt(6) + 1;
+		// int n2 = r.nextInt(6) + 1;
+		// int n3 = r.nextInt(6) + 1;
+		// int n4 = r.nextInt(6) + 1;
+		//
+		// if (n1 == 6) {
+		// count++;
+		// }
+		// if (n2 == 6) {
+		// count++;
+		// }
+		// if (n3 == 6) {
+		// count++;
+		// }
+		// if (n4 == 6) {
+		// count++;
+		// }
+		//
+		// if (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4) {
+		// System.out.println(n1 + " " + n2 + " " + n3 + " " + n4);
+		// continue;
+		// } else {
+		// System.out.println(n1 + " " + n2 + " " + n3 + " " + n4);
+		// break;
+		// }
+		// }
+		// System.out.println("6點出現的次數:" + count);
 	}
 
 }
