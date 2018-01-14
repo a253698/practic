b7af7d30f5378054fcad6efebf07e9fdfa750664
Mavis <angel253698@gamil.com>
2018-01-14 20:07:02 +0800

    PracticDiceRollerArray

--- src/com/mavis/PracticDiceRoller.java
+++ src/com/mavis/PracticDiceRoller.java
@@ -6,33 +6,56 @@ public class PracticDiceRoller {
 
 	public static void main(String[] args) {
 		
-			int count = 0;
-		
-		for (int i = 1; i < 10; i++) {
-			Random r = new Random();
-			int n1 = r.nextInt(6) + 1;
-			int n2 = r.nextInt(6) + 1;
-			int n3 = r.nextInt(6) + 1;
-			int n4 = r.nextInt(6) + 1;
+		int a[] = new int [ 6 +1];
+		for(int k=1; k<=10; k++) {
+			a[k] = k;
 			
-			if(n1 == 6) { count++;}
-			if(n2 == 6) { count++;} 
-			if(n3 == 6) { count++;} 
-			if(n4 == 6) { count++;} 
-
-			if (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4) {
-				System.out.println(n1 + " " + n2 + " " + n3 + " " + n4);
-				continue;
-			} else {
-				System.out.println(n1 + " " + n2 + " " + n3 + " " + n4);
-				break;
+			for(int j = 1; j <= 4; j++) {
+				
+				Random r = new Random();
+				a[k] = r.nextInt(6) + 1;
+				
+				System.out.print(a[k]);
+			
 			}
-			
+			System.out.print(" ");
 		}
 		
-		System.out.println("6點出現的次數:"+ count);
-		}
-
+		
+//
+//		int count = 0;
+//
+//		for (int i = 1; i < 10; i++) {
+//
+//			Random r = new Random();
+//
+//			int n1 = r.nextInt(6) + 1;
+//			int n2 = r.nextInt(6) + 1;
+//			int n3 = r.nextInt(6) + 1;
+//			int n4 = r.nextInt(6) + 1;
+//
+//			if (n1 == 6) {
+//				count++;
+//			}
+//			if (n2 == 6) {
+//				count++;
+//			}
+//			if (n3 == 6) {
+//				count++;
+//			}
+//			if (n4 == 6) {
+//				count++;
+//			}
+//
+//			if (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4) {
+//				System.out.println(n1 + " " + n2 + " " + n3 + " " + n4);
+//				continue;
+//			} else {
+//				System.out.println(n1 + " " + n2 + " " + n3 + " " + n4);
+//				break;
+//			}
+//		}
+//		System.out.println("6點出現的次數:" + count);
 	}
 
-
+}
